7ae3707f9a5160f06ab06dc1a2511828cf2306b9
HYEMIN <hyeminkim@korea.ac.kr>
2016-03-10 13:42:40 +0900

    dummy update

--- routes/ranking.js
+++ routes/ranking.js
@@ -8,24 +8,28 @@ router.get('/', function (req, res, next) {
                 "message": "친구 랭킹 요청에 성공하였습니다...",
                 "friends": [
                     {
-                        "friend_id": 2,
-                        "friend_name": "한솔1",
-                        "friend_hours": 19415
+                        "friend_id": 1,
+                        "friend_name": "채워니",
+                        "friend_hours": 19415,
+                        "friend_photourl":"https://s3.ap-northeast-2.amazonaws.com/fitmakerbucket/test/curri_moon.jpg"
                     },
                     {
                         "friend_id": 2,
-                        "friend_name": "한솔1",
-                        "friend_hours": 19415
+                        "friend_name": "서려니",
+                        "friend_hours": 19415,
+                        "friend_photourl":"https://s3.ap-northeast-2.amazonaws.com/fitmakerbucket/test/user_me_sul1.jpg"
                     },
                     {
                         "friend_id": 19,
-                        "friend_name": "handddoi",
-                        "friend_hours": 6810
+                        "friend_name": "떠려니",
+                        "friend_hours": 6810,
+                        "friend_photourl":"https://s3.ap-northeast-2.amazonaws.com/fitmakerbucket/test/user_me_sul2.jpg"
                     },
                     {
                         "friend_id": 7,
-                        "friend_name": "한솔2",
-                        "friend_hours": 270
+                        "friend_name": "숮이",
+                        "friend_hours": 270,
+                        "friend_photourl":"https://s3.ap-northeast-2.amazonaws.com/fitmakerbucket/test/user_me_suzy.jpg"
                     }
                 ]
             }
